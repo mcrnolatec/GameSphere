f3c10c34ed25546ae20aeda1e23b8bc04fab1ad3
dsrecec <86156532+dsrecec@users.noreply.github.com>
2024-02-28 18:35:04 +0100

    Added provideHttpClient(withFetch()) to providers to app.module.server

--- frontend/server.ts
+++ frontend/server.ts
@@ -27,7 +27,7 @@ export function app(): express.Express {
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
-
+    //ako req pocinje s rutama koje planiramo renderat izvrsi .render
     commonEngine
       .render({
         bootstrap: AppServerModule,
