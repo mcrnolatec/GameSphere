756697c01bd2acf03dfa1e36a10a728bd13b2a20
dsrecec <86156532+dsrecec@users.noreply.github.com>
2024-02-29 14:42:34 +0100

    Add gamesDAO.ts
    -Created a folder services where I placed gamesDAO.ts.ts which uses database.ts I created earlier and is responsible for making queries
    -created a method getGames that retrieves games from the database

---
+++ backend/src/services/dao/gamesDAO.ts
@@ -0,0 +1,18 @@
+const Batabase = require("../../database/database")
+
+export class GamesDao{
+    private database: any;
+
+    constructor() {
+        this.database = new Batabase("../../database/gameSphereDatabase.sqlite");
+    }
+
+    getGames = async () =>{
+        this.database.connectToDatabase();
+        let sql = "SELECT * FROM 'Games'";
+        let data = await this.database.performQuery(sql, []);
+        this.database.endConnection();
+        return data;
+    }
+}
+export default GamesDao;
